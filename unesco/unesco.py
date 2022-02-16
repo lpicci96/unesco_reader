a126f87936ba502dd131a7f969ffb3761354a783
lpicci96 <lpicci96@gmail.com>
2022-02-16 15:32:40 -0700

    adding scripts and data

--- unesco/unesco.py
+++ unesco/unesco.py
@@ -1,114 +1,69 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 10 16:34:11 2022
-
-@author: lpicc
 
 
 This script extracts data from the UNESCO Bulk download services here: https://apiportal.uis.unesco.org/bdds
 
-Functions:
-    read_zip - reads a csv from a zipped folder on the web without saving the file locally
-    datasets - returns available datasets to query
-    indicator_list - returns available indicators for a specific dataset
-    get_bulk - returns data for all indicators in a dataset
-    get_indicator - returns data for an indicator in a dataset
+
 """
 
-import requests
+
 import pandas as pd
-import io
-from zipfile import ZipFile
 from typing import Optional
+from unesco.utils import read_zip
 
 
-def read_zip(url:str, file_name:str) -> pd.DataFrame:
+def datasets():
     """
-    Reads a csv from the web contained in a zip folder
+    Metadata for available UNESCO datasets
     
-    Parameters
-    ----------
-    url : str
-        Zip folder url
-    file_name: str
-        CSV file name written as 'file_name.csv'
-        
     Returns
     -------
-    pandas dataframe object
+    pd.DataFrame
     """
     
-    try:
-        response = requests.get(url)
-        file = ZipFile(io.BytesIO(response.content))
-        if file_name not in list(file.NameToInfo.keys()):
-            raise ValueError(f'{file_name} is not found in the zipped folder')
-        df = pd.read_csv(file.open(file_name), low_memory=False)
-        
-        return df
+    return pd.read_csv('datasets.csv')
     
-    except ConnectionError:
-        raise ConnectionError('Could not read file')
 
-
-def datasets() -> pd.DataFrame:
+def indicators(dataset_code: str) -> pd.DataFrame:
     """
-    Returns a dataframe of available UNESCO datasets to query
-    along with a downloadable link for the bulk data
-    """
-    
-    base_url = 'https://apimgmtstzgjpfeq2u763lag.blob.core.windows.net/content/MediaLibrary/bdds/'
-    unesco_dict = {'dataset':['SDG Global and Thematic Indicators', 'Other Policy Relevant Indicators (OPRI)', 
-                              'Research and Development (R&D)', ' Innovation',
-                              'Cultural Employment', 'Feature Films', 'Cultural Trade', 'SDG 11',
-                              'Demographic and Socio-economic Indicators'],
-                   'code':['SDG', 'OPRI', 
-                           'SCI', 'INNO',
-                           'CLTE', 'FILM', 'CLTT', 'SDG11',
-                           'DEM'],
-                   'category':['Education', 'Education', 
-                               'Science', 'Science', 
-                               'Culture', 'Culture', 'Culture', 'Culture',
-                               'External']
-                   }
-    
-    unesco_dict['link'] = [base_url + code + '.zip' for code in unesco_dict['code']]
-    
-    return pd.DataFrame(unesco_dict)
+    Metadata for available indecators in a UNESCO dataset
 
-
-def indicator_list(dataset_code: str) -> pd.DataFrame:
-    """
-    Returns a dataframe with available indicators for a dataset
-    
-     Parameters
+    Parameters
     ----------
     dataset_code : str
-        Specify the code for the dataset. Call datasets() to see a dataframe of available datasets
+        Specify the UNESCO dataset code 
+
+    Returns
+    -------
+    pd.DataFrame
     """
-    
     ds = datasets()
     if dataset_code not in list(ds.code):
-        raise ValueError(f'{dataset_code} is not a valid code')
+        raise ValueError(f"{dataset_code} is not a valid code. Available datasets are {list(ds['code'])}")
     url = ds.loc[ds.code == dataset_code, 'link'].values[0]
     df = read_zip(url, file_name = f'{dataset_code}_LABEL.csv')
     
     return df
 
 
-def get_bulk(dataset_code: str, *, grouping: Optional[str] = 'NATIONAL') -> pd.DataFrame:
+def get_bulk(dataset_code:str, grouping: Optional[str] = 'NATIONAL') -> pd.DataFrame:
     """
-    Downloads data for all indicators in a dataset
-    
-     Parameters
+    Download data for all available indicators in a UNESCO dataset
+
+    Parameters
     ----------
     dataset_code : str
-        Specify the code for the dataset. run datasets() to see a dataframe of available datasets
-    
-    grouping: Optional[str]
-        specify the grouping level:
-            NATIONAL - country level grouping
-            REGIONAL - region level grouping
+        Specify the UNESCO dataset code 
+    grouping : str
+        Specify the geography grouping option
+        [NATIONAL] for country level grouping
+        [REGIONAL] for region level grouping. 
+        The default is [NATIONAL].
+
+    Returns
+    -------
+    pd.DataFrame
     """
     
     ds = datasets()
@@ -121,39 +76,42 @@ def get_bulk(dataset_code: str, *, grouping: Optional[str] = 'NATIONAL') -> pd.D
         
     url = ds.loc[ds.code == dataset_code, 'link'].values[0]
     df = read_zip(url, file_name=f'{dataset_code}_DATA_{grouping}.csv')
-    df = df.iloc[:, :4]
-    df.columns = ['code', 'country', 'year', 'value']
     
     return df
 
 
-def get_indicator(indicator_code:str, dataset_code: str, *, grouping: Optional[str] = 'NATIONAL') -> pd.DataFrame:
+def get_indicator(indicator_code: str, dataset_code: str, grouping: Optional[str] = 'NATIONAL') -> pd.DataFrame:
     """
-    Downloads an indicator from a UNESCO dataset
+    Download data for a specific UNESCO indicator
     
     Parameters
     ----------
-    indicator_code: str
-        specify the code for the indicator. Call indicator_list() for a list of available indicators and codes
-    
+    indicator_code : str
+        Specify the UNESCO indicator code.
     dataset_code : str
-        Specify the code for the dataset. run datasets() to see a dataframe of available datasets
-    
-    grouping: Optional[str]
-        specify the grouping level:
-            NATIONAL - country level grouping
-            REGIONAL - region level grouping
+        Specify the UNESCO dataset code .
+    grouping : str
+        Specify the geography grouping option
+        [NATIONAL] for country level grouping
+        [REGIONAL] for region level grouping. 
+        The default is [NATIONAL].
+
+    Returns
+    -------
+    pd.DataFrame.
+
     """
+   
+    bulk = get_bulk(dataset_code, grouping)
+    df = bulk[bulk['INDICATOR_ID'] == indicator_code].reset_index(drop=True)
     
-    df = get_bulk(dataset_code, grouping = grouping)
-    if len(df[df.code == indicator_code]) > 0:
-        df = df[df.code == indicator_code]
-        df = df.drop(columns='code')
+    #error handling if no data is returned
+    if len(df) == 0:
+        raise ValueError(f'No data available for {indicator_code}')
+        
+    return df
 
-        return df
 
-    else:
-        raise ValueError(f'{indicator_code} is invalid')
 
 
 
