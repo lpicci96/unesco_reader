b5e0a0da8ba615595c6fbce87bb72cd1bfb234be
lpicci96 <lpicci96@gmail.com>
2022-02-18 19:30:50 -0700

    adding tests

---
+++ tests/test_api.py
@@ -0,0 +1,67 @@
+"""tests for api.py"""
+
+
+import pytest
+from unesco_reader import api
+import pandas as pd
+
+
+test_dataset = 'SDR'
+test_indicator = ''
+errors = {'dataset_code': 'not_a_dataset',
+          'indicator_code': 'not_an_indicator',
+          'grouping': 'not_a_grouping'}
+
+def __loop_datasets(func, *args, **kwargs):
+    """
+    Loops all datasets through a function 
+    and checks that the output is not None and >0 
+    """
+    
+    ds = api.datasets()
+    if 'REGIONAL' in kwargs.values():
+        ds = ds[ds.regional]
+    
+    dataset_list = list(ds.code)
+    for code in dataset_list:
+        result = func(dataset_code=code, *args, **kwargs)
+        assert result is not None
+        assert len(result)>0
+    
+
+'''
+def test_datasets():
+    ds = api.datasets()
+    assert isinstance(ds, pd.core.frame.DataFrame)
+    assert ds is not None
+    assert len(ds)>0
+
+
+def test_indicators():
+    
+    not_a_dataset = 'not_a_real_dataset'
+    __loop_datasets(func = api.indicators)
+    
+    
+    with pytest.raises(ValueError):
+        indicator_df = api.indicators(not_a_dataset)
+        
+'''
+def test_get_bulk():
+    
+    
+    #__loop_datasets(func=api.get_bulk)
+    __loop_datasets(func=api.get_bulk, grouping='REGIONAL')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- unesco_reader/api.py
+++ unesco_reader/api.py
@@ -57,6 +57,7 @@ def datasets():
             "https://apimgmtstzgjpfeq2u763lag.blob.core.windows.net/content/MediaLibrary/bdds/SDG11.zip",
             "https://apimgmtstzgjpfeq2u763lag.blob.core.windows.net/content/MediaLibrary/bdds/DEM.zip",
         ],
+        'regional':[True, True, True, False, False, False, False, False, False]
     }
 
     return pd.DataFrame(dataset_dict)
@@ -112,7 +113,11 @@ def get_bulk(dataset_code: str, grouping: Optional[str] = "NATIONAL") -> pd.Data
         raise ValueError(f"{dataset_code} is not a valid code")
     if grouping not in ["NATIONAL", "REGIONAL"]:
         raise ValueError(f"{grouping} is not a valid grouping")
-
+    #if grouping is REGIONAL, check if it is available
+    if dataset_code not in list(ds.loc[ds.regional, 'code']):
+        raise ValueError(f'{dataset_code} does not have a REGIONAL option')
+        
+        
     url = ds.loc[ds.code == dataset_code, "link"].values[0]
     df = read_zip(url, file_name=f"{dataset_code}_DATA_{grouping}.csv")
 
