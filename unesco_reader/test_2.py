48732fe6111072d5032292b4c210fffbcb36f20a
lpicci96 <lpicci96@gmail.com>
2022-03-31 14:11:47 -0600

    Update test_2.py

--- unesco_reader/test_2.py
+++ unesco_reader/test_2.py
@@ -5,6 +5,7 @@ import requests
 import io
 from zipfile import ZipFile
 import warnings
+from tabulate import tabulate
 
 
 def read_csv(folder, file_name: str) -> pd.DataFrame:
@@ -18,7 +19,6 @@ datasets = pd.read_csv("datasets.csv")
 
 @dataclass
 class Fetcher:
-
     code: str = None
     grouping: str = 'NATIONAL'
 
@@ -56,6 +56,10 @@ class Fetcher:
             return read_csv(self.folder, f'{self.code}_REGION.csv')
 
     @property
+    def indicators(self):
+        return read_csv(self.folder, f'{self.code}_LABEL.csv')
+
+    @property
     def metadata(self):
         return (read_csv(self.folder, f'{self.code}_METADATA.csv')
                 .groupby(by=['INDICATOR_ID', 'COUNTRY_ID', 'YEAR', 'TYPE'], as_index=False)['METADATA']
@@ -70,10 +74,17 @@ class UIS(Fetcher):
     def __post_init__(self):
         super().__post_init__()
 
+    @property
+    def info(self):
+        print(
+            tabulate(pd.DataFrame({'category': ['name', 'code', 'category', 'available indicators', 'download link'],
+                                   'value': [self.name, self.code, self.category, len(self.indicators), self.url]}),
+                     tablefmt='github', showindex=False))
+
     def add_metadata(self):
         if self.grouping == 'NATIONAL':
             self.df = pd.merge(self.df, self.metadata,
-                               on = ['INDICATOR_ID', 'COUNTRY_ID', 'YEAR'],
+                               on=['INDICATOR_ID', 'COUNTRY_ID', 'YEAR'],
                                how='left')
         else:
             warnings.warn('Metadata is not available for regional data')
@@ -81,20 +92,19 @@ class UIS(Fetcher):
     def convert_countries(self, to: Optional[str] = 'name'):
         if to not in ['name', 'code']:
             raise ValueError(f'self{to} is not valid')
-        if self.grouping == 'REGIONAL':
+        elif self.grouping == 'REGIONAL':
             warnings.warn('Grouping is REGIONAL. No conversion available.')
+        elif to == 'name':
+            self.df['COUNTRY_ID'] = self.df['COUNTRY_ID'].map(dict(self.regions.values))
         else:
-            pass
+            self.df['COUNTRY_ID'] = (self.df['COUNTRY_ID']
+                                     .map({value: key for (key, value) in dict(self.regions.values).items()}))
 
-
-
-
-
-
-
-
-
-
-
-
-
+    def convert_indicators(self, to: Optional[str] = 'label'):
+        if to not in ['label', 'code']:
+            raise ValueError(f'self{to} is not valid')
+        elif to == 'label':
+            self.df['INDICATOR_ID'] = self.df['INDICATOR_ID'].map(dict(self.indicators.values))
+        else:
+            self.df['INDICATOR_ID'] = (self.df['INDICATOR_ID']
+                                       .map({value: key for (key, value) in dict(self.indicators.values).items()}))
