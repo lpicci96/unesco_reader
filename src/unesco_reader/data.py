78516ab5133a7c6c600e3e3fefc342ec4573202d
Luca Picci <lpicci96@gmail.com>
2024-10-06 18:26:24 +0200

    add basic structure to wrap api

---
+++ src/unesco_reader/data.py
@@ -0,0 +1,15 @@
+"""Module to get indicator data from the UIS API
+
+This module wraps the API endpoint to get indicator data from the UIS API.
+More information about this endpoint can be found at: https://api.uis.unesco.org/api/public/documentation/operations/getIndicatorData
+
+
+ENDPOINT: https://api.uis.unesco.org/api/public/data/indicators
+
+"""
+
+
+def get():
+    """Function to get indicator data from the UIS API"""
+
+    pass
